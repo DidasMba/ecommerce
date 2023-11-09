a33f0d3fae62689eb99fcd466969e51d4b69df04
DidasMba <daniel.mbarushi@gmail.com>
2023-11-10 02:20:11 +0300

    add cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,37 @@
 // Cart.js
+import React from 'react';
+
+const Cart = ({ cart, increaseQuantity, decreaseQuantity }) => {
+  const calculateTotal = () => {
+    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
+
+  return (
+    <div className="cart-container">
+      <h2>Shopping Cart</h2>
+      <div className="cart-items">
+        {cart.map((item) => (
+          <div key={item.id} className="cart-item">
+            <p>{item.title}</p>
+            <p>Price: ${item.price}</p>
+            <p>Quantity: {item.quantity}</p>
+            <p>Total: ${item.price * item.quantity}</p>
+            <button className="red-button" onClick={() => increaseQuantity(item)}>+</button>
+            <button onClick={() => decreaseQuantity(item)}>-</button>
+          </div>
+        ))}
+      </div>
+      <p>Total: ${calculateTotal()}</p>
+    </div>
+  );
+};
+
+export default Cart;
+
+
+
+
+// Cart.js
 /*import React from 'react';
 
 const Cart = ({ cart, increaseQuantity, decreaseQuantity }) => {
