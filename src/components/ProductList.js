66806e523a8d276164dc19283719a39221e74f13
DidasMba <daniel.mbarushi@gmail.com>
2023-11-10 02:18:25 +0300

    add productlist

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,30 @@
 // ProductList.js
+import React from 'react';
+
+const ProductList = ({ products, addToCart }) => {
+  return (
+    <div className="product-list-container">
+      <h2>Product List</h2>
+      <div className="product-grid">
+        {products.map((product) => (
+          <div key={product.id} className="product-item">
+            <img src={product.image} alt={product.title} />
+            <p>{product.title}</p>
+            <p>${product.price}</p>
+            <button className="red-button" onClick={() => addToCart(product)}>+</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
+
+
+
+
+// ProductList.js
 /*import React from 'react';
 
 const ProductList = ({ products, addToCart }) => {
