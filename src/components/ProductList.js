819f72400cb3c920c835a1cd7ac09ed84370ee20
DidasMba <daniel.mbarushi@gmail.com>
2023-11-10 02:58:07 +0300

    overflow

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,44 @@
 // ProductList.js
-import React from 'react';
+import React, { useState } from 'react';
+
+const ProductList = ({ products, addToCart }) => {
+  const [activeProduct, setActiveProduct] = useState(null);
+
+  // Gère le clic sur un produit
+  const handleProductClick = (product) => {
+    addToCart(product); // Ajoute le produit au panier
+    setActiveProduct(product.id); // Active le produit (pour l'effet de style)
+  };
+
+  return (
+    <div className="product-list-container">
+      <h2>Product List</h2>
+      <div className="product-grid">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className={`product-item ${activeProduct === product.id ? 'active' : ''}`}
+            onClick={() => handleProductClick(product)}
+          >
+            <img src={product.image} alt={product.title} />
+            <p>{product.title}</p>
+            <p>${product.price}</p>
+            <button className="red-button" onClick={() => addToCart(product)}>
+              +
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
+
+
+
+// ProductList.js
+/*import React from 'react';
 
 const ProductList = ({ products, addToCart }) => {
   return (
@@ -19,7 +58,7 @@ const ProductList = ({ products, addToCart }) => {
   );
 };
 
-export default ProductList;
+export default ProductList;*/
 
 
 
