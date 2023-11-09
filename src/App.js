e1246df31772c0a37f070bbc40b866f9dd9e1810
DidasMba <daniel.mbarushi@gmail.com>
2023-11-10 02:16:21 +0300

    add appjs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,96 @@
+// App.js
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import './App.css';
+
+const App = () => {
+  const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const addToCart = (product) => {
+    const existingItem = cart.find((item) => item.id === product.id);
+
+    if (existingItem) {
+      setCart(
+        cart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const increaseQuantity = (item) => {
+    setCart(
+      cart.map((cartItem) =>
+        cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+      )
+    );
+  };
+
+  const decreaseQuantity = (item) => {
+    if (item.quantity > 1) {
+      setCart(
+        cart.map((cartItem) =>
+          cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
+        )
+      );
+    }
+  };
+
+  const calculateTotal = () => {
+    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
+
+  return (
+    <Router>
+      <div className="app-container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route
+            path="/"
+            element={<ProductList products={products} addToCart={addToCart} />}
+          />
+          <Route
+            path="/cart"
+            element={<Cart cart={cart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} />}
+          />
+        </Routes>
+        <div className="total-container">
+          <p>Total: ${calculateTotal()}</p>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
 
 
 
