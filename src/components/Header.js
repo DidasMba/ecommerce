07684650bb7eca9b26f9fdb4c7736bdd08ca32db
DidasMba <daniel.mbarushi@gmail.com>
2023-11-10 18:35:19 +0300

    add slidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 // Header.js
 //import React from 'react';
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
 
 const Header = () => {
   return (
@@ -18,4 +20,41 @@ const Header = () => {
   );
 };
 
+export default Header;*/
+
+// Header.js
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'; // Importez l'icône de valise de la bibliothèque Material-UI
+
+const Header = () => {
+  const [isCartOpen, setCartOpen] = useState(false);
+
+  const toggleCart = () => {
+    setCartOpen(!isCartOpen);
+  };
+
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <div className="cart-icon" onClick={toggleCart}>
+        <ShoppingCartIcon />
+      </div>
+      {isCartOpen && (
+        <div className="cart-sidebar">
+          {/* Contenu du panier, y compris les informations du vêtement, le prix total, etc. */}
+          {/* ... */}
+        </div>
+      )}
+    </header>
+  );
+};
+
 export default Header;
+
+
+
